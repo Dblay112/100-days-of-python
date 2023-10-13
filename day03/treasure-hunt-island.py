d1dbc76582ae4cac0b5b2ba059f624b8df0dc299
dblay112 <brightdjogatse@gmail.com>
2023-10-13 17:26:26 +0000

    updated file

--- day03/treasure-hunt-island.py
+++ day03/treasure-hunt-island.py
@@ -26,20 +26,20 @@ print('''
                                                /______/______/______/______/______/______/______/______/______/______/_____
                                                /
                                                *******************************************************************************''')
+print("Welcome to Treasure Island")
 print("Your mission is to find the treasure.")
 crossroad = input('You\'re at a cross road. Where do you want to go? Type "left" or "right"').lower()
 if crossroad == "left":
-        swim_choice = input('You\'ve come to a lake. There is an island in the middle of the lake Type "wait" to wait
-        for a boat or type "swim" to swim accross.').lower()
-            if swim_choice == "wait":
-                        door_choice = input('You arrive at the island unharmed. There is a house with 3 doors. One red,
-                        one yellow and one blue. Which colour do you choose?').lower()
-                                if door_choice == "yellow":
-                                                print("You Win!!!")
-                                                        elif door_choice == "red" or "blue":
-                                                                        print("You entered a room of beast. Game
-                                                                        Over!!!")
-                                                                            elif swim_choice == "swim":
-                                                                                        print("Game Over!!!")
-                                                                                    else:
-                                                                                        print("Game Over!!!")
+    swim_choice = input('You\'ve come to a lake. There is an island in the middle of the lake Type "wait" to wait for a
+    boat or type "swim" to swim accross.').lower()
+    if swim_choice == "wait":
+        door_choice = input('You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and
+        one blue. Which colour do you choose?').lower()
+        if door_choice == "yellow":
+            print("You Win!!!")
+        elif door_choice == "red" or "blue":
+                        print("You entered a room of beast. Game Over!!!")
+    elif swim_choice == "swim":
+        print("Game Over!!!")
+else:
+    print("Game Over!!!")
