f3d51b28ac260fa284487f77aa6258ad880cc750
Bright Djogatse <125486553+Dblay112@users.noreply.github.com>
2023-10-25 15:49:03 +0000

    Update rock-paper-scissors.py

--- day04/rock-paper-scissors.py
+++ day04/rock-paper-scissors.py
@@ -25,3 +25,18 @@ scissors = '''
 ---.__(___)
 '''
 
+game_choice = [rock, paper, scissors]
+user_choice = int(input("What do yo choose? Type 0 for Rock, 1 for Paper or 2 for Scissors\n"))
+print(game_choice[user_choice])
+
+comp_choice = random.randint(0, 2)
+print("Computer chose: ")
+print(game_choice[comp_choice])
+if user_choice >= 3 or user_choice < 0:
+  print("Invalid Choice, You lose!")
+elif comp_choice > user_choice:
+  print("You Lose!!!")
+elif user_choice == 2 and comp_choice == 0:
+  print("You Lose")
+elif comp_choice == 0 and comp_choice == 2:
+  print("You Win!!!")
